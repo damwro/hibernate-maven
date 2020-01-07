b6701d487169f81121c531bff9fb88f613ba1bd8
dwr <damian.wrobel.1990@gmail.com>
2020-01-07 18:44:39 +0100

    add compiler source, add JDBCConnectionClass

---
+++ src/main/java/pl/sda/jdbc/JDBCConnection.java
@@ -0,0 +1,26 @@
+package pl.sda.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCConnection {
+
+    public static void main(String[] args) {
+        //strefa czasowa oraz wyłączenie SSL
+        String jdbcUrl = "jdbc:mysql://localhost:3306/student_sda?useSSL=false&serverTimezone=UTC";
+        String user = "student";
+        String password = "student";
+
+        try {
+            System.out.println("łaczenie z bazą danych ");
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+
+            System.out.println("połączyłeś się z bazą danych");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+    }
+}
